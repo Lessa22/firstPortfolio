e872caa6de51b3d07d4dc64a0923b9bd692438cf
Lessa22 <andriatahianasandratra@gmail.com>
2026-07-28 18:09:26 +0300

    Design2

--- src/components/background/Background.jsx
+++ src/components/background/Background.jsx
@@ -1,17 +1,23 @@
-import Particles from './Particles'
-import styles from './Background.module.css'
+import Particles from "./Particles";
+import styles from "./Background.module.css";
 
 function Background() {
   return (
-    <div className={styles.background} aria-hidden="true">
-      <div className={styles.gradient} />
+    <div className={styles.background}>
+      <div className={styles.gradient}></div>
+
       <div className={`${styles.blob} ${styles.blobOne}`} />
       <div className={`${styles.blob} ${styles.blobTwo}`} />
       <div className={`${styles.blob} ${styles.blobThree}`} />
+
+      {/* Nouvelle couche de lumière */}
+      <div className={styles.liquid}></div>
+
       <Particles />
-      <div className={styles.noise} />
+
+      <div className={styles.noise}></div>
     </div>
-  )
+  );
 }
 
-export default Background
+export default Background;
